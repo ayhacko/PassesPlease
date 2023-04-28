a5b7b9aa2d45c55b339128027c73b42adeb084b5
ayhacko <ayhuko2005@gmail.com>
2023-04-28 12:50:37 -0400

    Pushed for woddd

--- Frontend/Document.java
+++ Frontend/Document.java
@@ -1,5 +1,42 @@
 package Frontend;
 
-public abstract class Document {
+import java.awt.*;
+import java.awt.image.*;
 
+public abstract class Document {
+    private BufferedImage image;
+    private Coordinate position;
+    private boolean expanded;
+
+    public Document(BufferedImage image, Coordinate position) {
+        this.image = image;
+        expanded = false;
+    }
+
+    public BufferedImage getImage() {
+        return image;
+    }
+
+    public Coordinate getPosition() {
+        return position;
+    }
+
+    public void setPosition(Coordinate position) {
+        this.position = position;
+    }
+
+    public boolean getExpanded() {
+        return expanded;
+    }
+
+    public void expand() {
+        expanded = true;
+    }
+
+    public void minimize() {
+        expanded = false;
+    }
+
+    public abstract boolean onComponent(Coordinate coordinate);
+    public abstract void draw(Graphics2D g);
 }
--- Frontend/StudentID.java
+++ Frontend/StudentID.java
@@ -1,5 +1,22 @@
 package Frontend;
 
-public class StudentID {
+import java.awt.*;
+import java.awt.image.*;
+import Resources.Images.*;
 
+public class StudentID extends Document {
+    public final BufferedImage image = Images.loadImage();
+    public StudentID(Coordinate position) {
+        super();
+    }
+
+    @Override
+    public boolean onComponent(Coordinate coordinate) {
+        return false;
+    }
+
+    @Override
+    public void draw(Graphics2D g) {
+
+    }
 }
