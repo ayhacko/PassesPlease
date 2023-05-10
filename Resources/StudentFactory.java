6b34ad9126d9f35fc790a8b88801d55b3ef5ac62
SZ-3300 <stephen.zhao3300@gmail.com>
2023-05-09 20:35:54 -0400

    OHYEAH

--- Resources/StudentFactory.java
+++ Resources/StudentFactory.java
@@ -1,5 +1,9 @@
 package Resources;
 
+import Backend.Document;
+
+import java.util.ArrayList;
+
 public class StudentFactory {
     public static String generateName() {
         return "";
@@ -18,5 +22,74 @@ public class StudentFactory {
         int month = (int) (Math.random() * 12) + 1;
         int day = (int) (Math.random() * 28) + 1;
         return year + "-" + month + "-" + day;
+    }
+
+    public static String generateHeight(){
+        int height = (int) (Math.random() * 100) + 30;
+        return height + "";
+    }
+
+
+    public static String generateWeight(){
+        int weight = (int) (Math.random() * 100) + 100;
+        return weight + "";
+    }
+
+    public static String idExpiration(){
+        int year = 2089;
+        int month = (int) (Math.random() * 5) + 8;
+        int day = (int) (Math.random() * 28) + 1;
+        return year + "-" + month + "-" + day;
+    }
+
+    public static String generatePermitDay(){
+        int number = (int) Math.round((Math.random()));
+        return number + "";
+
+    }
+
+    public static ArrayList<String> generateDiseases(){
+        ArrayList<String> diseases = new ArrayList<String>();
+        int number = 0;
+
+        for(int i = 0; i<3; i++){
+            number = (int) (Math.random()*5)+1;
+            if(number == 1){
+                if(i == 0){
+                    diseases.add("Bogovirus");
+                }else if(i == 1){
+                    diseases.add("Yumyum");
+                }else{
+                    diseases.add("Mad Cow Disease");
+                }
+            }
+        }
+
+        return diseases;
+
+    }
+
+
+    public static ArrayList<Document> generateDocument(){
+        ArrayList<Document> document = new ArrayList<Document>();
+        int number = 0;
+
+        for(int i = 0; i<4; i++){
+            number = (int) (Math.random()*1)+1;
+            if(number == 1){
+                if(i == 0){
+
+                }else if(i == 1){
+
+                }else if(i == 2){
+
+                }else{
+
+                }
+            }
+        }
+
+        return document;
+
     }
 }
