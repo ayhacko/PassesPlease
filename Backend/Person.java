7bf7813d5f5e96d1de115e277719e8f64aad63b0
ayhacko <ayhuko2005@gmail.com>
2023-05-10 12:43:36 -0400

    Get instantiated b**** (tried again again)

--- Backend/Person.java
+++ Backend/Person.java
@@ -67,7 +67,7 @@ public abstract class Person {
     public boolean letThrough() {
         return letThrough;
     }
-
+//
     public void setLetThrough(boolean letThrough) {
         this.letThrough = letThrough;
     }
